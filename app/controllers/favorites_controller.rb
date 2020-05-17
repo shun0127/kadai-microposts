abdb5bb875fc361fb1c25f61f8a46f30d3f9152d
shun <test@gm.co.jp>
2020-05-17 09:57:45 +0000

    favorite update1

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,16 +2,23 @@ class FavoritesController < ApplicationController
   before_action :require_user_logged_in
   def create
     add_micropost = Micropost.find(params[:micropost_id])
+    micropost_user = User.find(add_micropost.user_id)
     current_user.add_favorite(add_micropost)
     flash[:success] = "お気に入りに追加しました。"
-    redirect_to likes_user_path(current_user)
+    redirect_to micropost_user
   end
 
   def destroy
     rm_micropost = Micropost.find(params[:micropost_id])
-#    micropost_user = User.find(rm_micropost.user_id)
+    micropost_user = User.find(rm_micropost.user_id)
     current_user.rm_favorite(rm_micropost)
     flash[:success]="お気に入りから削除しました。"
-    redirect_to likes_user_path(current_user)
+    if params[:message_from] == "from_micropost"
+      redirect_to micropost_user
+    elsif params[:message_from] == "from_favorites_list"
+      redirect_to likes_user_path(current_user)
+    else
+      redirect_to root_url
+    end
   end
 end
